22827419789953c056c86be5c8337201372dec15
Jose Fernando Zapata Zambrano <joferzz1989@gmail.com>
2019-11-07 12:25:41 -0400

    :jack_o_lantern: feat: finish
    - watcher mods
    - observe

---
+++ observe.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = scenario;
+
+var replify = require("replify");
+
+function scenario(callback) {
+  var createServer = require("http").createServer;
+
+  var server = createServer();
+  server.__message = "REPLs are neat";
+
+  var repl = replify({
+    name: 'bug-clinic'
+  }, server);
+
+  server.listen(8080, () => {
+      callback(server, repl);
+  })
+}
--- watcher.js
+++ watcher.js
@@ -2,18 +2,12 @@
 
 module.exports = {
     local : function (traces) {
-        console.error('up');
-        let count = 0;
-
         traces.on("request:start", function (trace) {
             console.log(trace.url);
         });
 
         traces.on("request:end", function (trace) {
-            count++;
             console.error(trace.statusCode);
-            if(count===2)
-                process.exit(0)
         });
     }
 };
